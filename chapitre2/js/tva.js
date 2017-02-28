c9d550e0b1b0c8eef1d5f7e84025a0b4a436172f
simplon <rudy__10@hotmail.fr>
2017-02-28 21:09:48 +0100

    modification calcul

--- chapitre2/js/tva.js
+++ chapitre2/js/tva.js
@@ -1,5 +1,6 @@
 var ht=prompt("saisisez un chiffre ");
-var tva = ht * 19,6/100;
-var ttc = ht + tva;
+var tva = 1.196;
+var ttc = ht *tva;
+
 alert("le prix TTC est" + "" + ttc);
 console.log("le prix TTC est" + "" + ttc);
