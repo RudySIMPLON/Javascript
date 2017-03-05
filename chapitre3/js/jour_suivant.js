e3f646f3b4079839ae9994f0bef8961358d0a590
simplon <rudy__10@hotmail.fr>
2017-03-05 13:44:45 +0100

    exo jour suivant

---
+++ chapitre3/js/jour_suivant.js
@@ -0,0 +1,22 @@
+var meteo = prompt("qu'elle jour somme nous ?");
+
+if(meteo === "lundi"){
+	console.log("Demain nous seront mardi")
+}
+else if(meteo === "mardi"){
+	console.log("Demain nous seront mercredi")
+} else if (meteo === "mercredi"){
+	console.log("Demain nous seront jeudi")
+} else if (meteo === "jeudi"){  
+console.log("Demain nous seront vendredi")
+}else if (meteo === "vendredi"){  
+console.log("Demain nous seront samedi")
+}else if (meteo === "samedi"){  
+console.log("Demain nous seront dimanche")
+}else if (meteo === "dimanche")
+{  
+console.log("Demain nous seront lundi")
+}
+
+ else console.log("Comprenez vous le francais ?");
+
