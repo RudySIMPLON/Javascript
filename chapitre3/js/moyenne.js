8c0986fa219952374d6eb6274800207073f65cb3
simplon <rudy__10@hotmail.fr>
2017-03-05 14:58:10 +0100

    resulstat baccalaur&at

---
+++ chapitre3/js/moyenne.js
@@ -0,0 +1,13 @@
+
+
+var nbr = Number(prompt("saissisez un nombre"));
+if (nbr < 10){
+	console.log("Vous etes recalé")
+}
+	else if (nbr <12){
+
+	console.log("vous etes reçu");
+	}
+else if (nbr >= 12){
+	console.log("vous etes reçu ave la mention bien")
+}
