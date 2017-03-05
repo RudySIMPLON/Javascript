c041b8a2c9c69c97b46a299bbd25cba7a4a29b68
simplon <rudy__10@hotmail.fr>
2017-03-05 14:06:47 +0100

    comparaison nombre

---
+++ chapitre3/js/comparaison.js
@@ -0,0 +1,12 @@
+var nbr1 = Number(prompt("saisissez un nombre"));
+var nbr2 = Number(prompt("saisissez un nombre"));
+
+if (nbr1 <nbr2){
+	console.log(nbr1 +" est plus petit que "+nbr2)
+}
+else if (nbr1 > nbr2){
+	console.log(nbr1+" est plus grand que "+nbr2)
+} else if(nbr1=nbr2){
+	console.log(nbr1+" est egal à "+nbr2)
+}
+else console.log("etes vous ilétre");
