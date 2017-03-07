8fc3a967ec6897b9b9febb8b4f801216fa51bfb1
simplon <rudy__10@hotmail.fr>
2017-03-07 23:33:40 +0100

    nombre de jours dans un mois

---
+++ chapitre3/js/nombre_jour.js
@@ -0,0 +1,22 @@
+var jour = Number(prompt("rentre un nombre"));
+
+
+if (jour=== 3 || jour=== 5 || jour ===7 || jour ===8|| 
+
+	jour ===10|| jour ===12){
+
+	console.log("il y a 31 jour dans ce mois")
+
+	
+} else if (jour === 4 || jour ===6|| jour ===9|| jour ===11){
+	console.log("il y a 30");
+} else if (jour===2){
+	console.log("il y a 28 jours");
+} else {
+	console.log("mois non reconnu")
+}
+
+
+
+// 46911
+// 2
